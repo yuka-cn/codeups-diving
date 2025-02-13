2387b34ee8e80ddfedeb584be8bb4cae94ec8a15
yuka <yuka.68cn@gmail.com>
2025-02-14 05:25:29 +0900

    header

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+  //ドロワーメニュー
+$(".js-hamburger").click(function () {
+  if ($(".js-hamburger").hasClass("is-active")) {
+    $(".js-hamburger").removeClass("is-active");
+    // $("html").toggleClass("is-fixed");
+    $(".js-sp-nav").fadeOut(300);
+  } else {
+    $(".js-hamburger").addClass("is-active");
+    $(".js-sp-nav").fadeIn(300);
+  }
+});
 
 });
