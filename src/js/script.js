e2353526e59945dddf5bcfadb5e6f2de473bc31c
yuka <yuka.68cn@gmail.com>
2025-02-18 02:59:00 +0900

    topへ戻るボタン

--- src/js/script.js
+++ src/js/script.js
@@ -107,7 +107,35 @@ jQuery(function ($) {
       }
     });
   });
+
+  // topへ戻るボタン
+  let topBtn = $(".c-to-top");
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
 });
 
-console.log(typeof jQuery);
+
 
